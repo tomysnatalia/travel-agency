91f50e765952c4f83c8ea3b457483776b6fa71ac
tomysnatalia <hanf.natalia@gmail.com>
2020-08-17 18:35:42 +0200

    commit

--- src/main/java/com/crud/travel/agency/controller/ReservationController.java
+++ src/main/java/com/crud/travel/agency/controller/ReservationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-//@CrossOrigin("*")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v3")
 public class ReservationController {
